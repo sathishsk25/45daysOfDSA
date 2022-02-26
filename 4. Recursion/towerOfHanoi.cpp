c75e01284fc6f48322fe393f73f80c5e6640edb7
SATHISH KUMAR <69998192+sathishsk25@users.noreply.github.com>
2022-02-26 23:26:07 +0530

    #day13

---
+++ 4. Recursion/towerOfHanoi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int TOH(int n, int A, int B, int C){
+    if(n>0){
+        TOH(n-1,A,C,B);
+        cout << A << " " <<  C << ", ";
+        TOH(n-1,B,A,C);
+    }
+}
+
+int main(){
+    int n=3;
+    // cin >> n;
+    TOH(n,1,2,3);
+}
