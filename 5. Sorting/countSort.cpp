8c7a1d586885e58a61b610a9e99face29281f736
SATHISH KUMAR <69998192+sathishsk25@users.noreply.github.com>
2022-03-01 20:11:26 +0530

    #day15

--- 5. Sorting/countSort.cpp
+++ 5. Sorting/countSort.cpp
@@ -35,6 +35,7 @@ void countSort(int arr[],int n){
 int main(){
     int arr[] = {4,6,2,8,1,3,9,7,5};
     int len = sizeof(arr)/sizeof(arr[1]);
+    
     countSort(arr,len);
     for(int i=0; i<len; i++)
     cout << arr[i] << " ";
