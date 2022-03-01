bf0397dceeef363c1ac40371787996025b80f151
SATHISH KUMAR <69998192+sathishsk25@users.noreply.github.com>
2022-03-01 20:06:42 +0530

    #day16

---
+++ 5. Sorting/countSort.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int findMax(int arr[],int n){
+    int max=arr[0];
+    for(int i=1; i<n; i++){
+        if(arr[i]>max)
+        max = arr[i];
+    }
+    return max;
+}
+
+void countSort(int arr[],int n){
+    int *c;
+    int max = findMax(arr,n);
+    c = new int[max+1];
+    for(int i=0; i<max+1; i++){
+        c[i]=0;
+    }
+    for(int i=0; i<=max; i++){
+        c[arr[i]]++;
+    }
+    int i=0,j=0;
+    while(i<=max){
+        if(c[i]>0){
+            arr[j++]=i;
+            c[i]--;
+        }
+        else
+        i++;
+    }
+}
+
+
+int main(){
+    int arr[] = {4,6,2,8,1,3,9,7,5};
+    int len = sizeof(arr)/sizeof(arr[1]);
+    countSort(arr,len);
+    for(int i=0; i<len; i++)
+    cout << arr[i] << " ";
+}
---
+++ 5. Sorting/mergeSort.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+void merge(int arr[],int l, int mid, int h){
+    int arr2[h+1];
+    int i,j,k;
+    i=l; k=l; j=mid+1;
+    while(i<=mid && j<=h){
+        if(arr[i]>arr[j])
+        arr2[k++] = arr[j++];
+        else
+        arr2[k++] = arr[i++];
+    }
+    for(; i<=mid; i++)
+    arr2[k++]=arr[i];
+    for(; j<=h; j++)
+    arr2[k++]=arr[j];
+
+    for(int i=l; i<=h; i++)
+    arr[i] = arr2[i];
+}
+
+void mergeSortI(int arr[],int n){
+    int p,i,l,mid,h;
+    for(p=2; p<=n; p=p*2){
+        for(i=0; i+p-1<n; i=i+p){
+            l=i;
+            h=i+p-1;
+            mid=(l+h)/2;
+            merge(arr,l,mid,h);
+        }
+    }
+    if(p/2<n)
+    merge(arr,0,p/2-1, n);
+}
+
+void mergeSortR(int arr[],int l, int h){
+    int mid;
+    if(l<h){
+        mid = (l+h)/2;
+        mergeSortR(arr,l,mid);
+        mergeSortR(arr,mid+1,h);
+        merge(arr,l,mid,h);
+    }
+}
+int main(){
+    int arr[] = {4,6,2,8,1,3,9,7,5};
+    int len = sizeof(arr)/sizeof(arr[1]);
+    mergeSortI(arr,len);
+    mergeSortR(arr,0,len);
+    for(int i=0; i<len; i++)
+    cout << arr[i] << " ";
+}
