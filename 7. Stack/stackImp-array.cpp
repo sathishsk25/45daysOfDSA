3dfd4e460d508122ceb35a0cc82b2729c2e7870b
SATHISH KUMAR <69998192+sathishsk25@users.noreply.github.com>
2022-03-07 23:54:12 +0530

    #day20

--- 7. Stack/stackImp-array.cpp
+++ 7. Stack/stackImp-array.cpp
@@ -18,7 +18,7 @@ void display(struct Stack st){
     int i;
     for(i=st.top; i>=0; i--)
     cout << st.S[i] << " ";
-    cout << endl;
+    cout << endl; 
 }
 
 void push(struct Stack *st, int x){
@@ -35,18 +35,48 @@ int pop(struct Stack *st){
     if(st->top == -1)
     cout << "Stack underflow";
     else{
-        x = st->S[st->top--];
+        x = st->S[st->top];
+        st->top--;
     }
     return x;
 }
 
+int peek(struct Stack st, int i){
+ int x = -1;
+ if(st.top-i+1<0)
+ cout << "invalid index";
+ else
+ x = st.S[st.top-i+1];
+ return x;
+}
+
+int isEmpty(struct Stack st){
+    if(st.top==-1) return 1;
+    else return 0;
+}
+
+int isFull(struct Stack st){
+    if(st.top == st.size-1) return 1;
+    else return 0;
+}
+
+int stackTop(struct Stack st){
+    if(!isEmpty(st))
+    return st.S[st.top];
+    return -1;
+}
+
 int main(){
-    struct Stack st;
+    struct Stack st;5
     create(&st);
 
     push(&st, 10);
     push(&st, 20);
     push(&st, 30);
+    push(&st, 40);
 
+    cout << "Deleted ele : " << pop(&st) << endl;
+    cout << "peek ele : " << peek(st,2) << endl;
+    cout << "top ele : " << stackTop(st) << endl;
     display(st);
 }
