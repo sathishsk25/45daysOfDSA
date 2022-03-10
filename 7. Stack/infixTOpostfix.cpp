38a2fd71b3bef42d72199eb0b8f541f8c43ca410
SATHISH KUMAR <69998192+sathishsk25@users.noreply.github.com>
2022-03-10 22:06:09 +0530

    #day22

---
+++ 7. Stack/infixTOpostfix.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+struct Node{
+    char data;
+    struct Node *next;
+}*top=NULL;
+
+void push(char x){
+    struct Node *t;
+    t= new Node;
+
+    if(t==NULL)
+    cout << "Stack is full\n";
+    else{
+        t->data = x;
+        t->next = top;
+        top = t;
+    }
+}
+
+char pop(){
+    struct Node *t;
+    char x=-1;
+    if(top==NULL)
+    cout << "stack is empty";
+    else{
+        t=top;
+        top=top->next;
+        x=t->data;
+        delete t;
+    }
+    return x;
+}
+
+void display(){
+    struct Node *p;
+    p = top;
+    while(p!=NULL){
+    cout << p->data << " ";
+    p=p->next;
+    }
+    cout << endl;
+}
+
+int isBalanced(char *exp){
+    for(int i=0; exp[i]!='\0'; i++){
+        if( exp[i]=='(' )
+            push(exp[i]);
+        else if(exp[i]==')'){
+            if(top==NULL)
+            return 0;
+            else
+            pop();
+        }
+    }
+    if(top==NULL)
+    return 1;
+    else
+    return 0;
+}
+
+int pre(char x){
+    if(x=='+' || x=='-') 
+        return 1;
+    else if(x=='*' || x=='/')
+        return 2;
+    return 0;
+}
+
+
+int isOperand(char x){
+    if(x=='+' || x=='-' || x=='*' || x=='/')
+        return 0;
+    else
+        return 1;
+}
+
+char *inTOpost(char *infix){
+    int i=0,j=0;
+    char *postfix;
+    int len = strlen(infix);
+    postfix = new char(len + 2);
+
+    while(infix[i] != '\0'){
+        if(isOperand(infix[i]))
+        postfix[j++] = infix[i++];
+        else{
+            if(pre(infix[i])>pre(top->data))
+            push(infix[i++]);
+            else
+            postfix[j++]=pop();
+        }
+    }
+
+    while(top!=NULL){
+        postfix[j++]=pop();
+    }
+    postfix[j]='\0';
+    return postfix;
+}
+
+int main()
+{
+    char *infix=(char*)"a+b*c-d/e";
+    char *postfix=inTOpost(infix);
+    cout << postfix;
+    return 0;
+}
